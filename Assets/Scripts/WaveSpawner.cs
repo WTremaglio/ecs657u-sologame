a4666e81abe0f27798ea7841f333be73b83f1270
William Tremaglio <w.tremaglio@se20.qmul.ac.uk>
2024-12-20 12:19:14 -0500

    v0.0.3
    Wave Spawner

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5.5f;
+    private float countdown = 2f;
+
+    public TMP_Text waveCountdownText;
+
+    private int waveIndex = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
