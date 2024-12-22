503c1ab54bddd4b04bd6989b7b1e85643952e498
William Tremaglio <w.tremaglio@se20.qmul.ac.uk>
2024-12-21 20:47:28 -0500

    v0.1.6
    Slowing

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,36 +2,23 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 10f;
+    public float startSpeed = 10f;
+    [HideInInspector]
+    public float speed;
 
-    public int health = 100;
+    public float health = 100;
 
-    public int value = 50;
+    public int worth = 50;
 
     public GameObject deathEffect;
-
-    private Transform target;
-    private int wavepointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        target = Waypoints.points[0];
+        speed = startSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
-
-        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
-        {
-            GetNextWaypoint();
-        }
-    }
-
-    public void TakeDamage(int amount)
+    public void TakeDamage(float amount)
     {
         health -= amount;
 
@@ -41,31 +28,18 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    public void Slow(float pct)
+    {
+        speed = startSpeed * (1f - pct);
+    }
+
     void Die()
     {
-        PlayerStats.Money += value;
+        PlayerStats.Money += worth;
         
         GameObject effect = (GameObject)Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(effect, 5f);
 
-        Destroy(gameObject);
-    }
-
-    void GetNextWaypoint()
-    {
-        if (wavepointIndex >= Waypoints.points.Length - 1)
-        {
-            EndPath();
-            return;
-        }
-
-        wavepointIndex++;
-        target = Waypoints.points[wavepointIndex];
-    }
-
-    void EndPath()
-    {
-        PlayerStats.Lives--;
         Destroy(gameObject);
     }
 }
